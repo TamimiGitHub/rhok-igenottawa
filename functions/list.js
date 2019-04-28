9ef29aa52657e842bb6e37b2894ce38b9eb9fea1
Tai Dickerson <cassidy@Voyager.local>
2019-04-28 00:41:20 -0400

    Make list function open its own connection

--- functions/list.js
+++ functions/list.js
@@ -1,5 +1,18 @@
+require('dotenv').config()
 const mongoose = require('mongoose');
 const EventItem = require('./models/eventItem.js')
+
+let uri = process.env.CONNECTION_STRING;
+//Connect to db
+mongoose.
+  connect(uri, {
+    bufferCommands: false,
+    bufferMaxEntries: 0,
+    useNewUrlParser: true
+  })
+  .then(() => console.log("Connection to MongoDB successful"))
+  .catch(err => console.error("Failed to connect to MongoDB"))
+ 
  
 exports.handler = function(event, context, callback) {
   // see https://mongoosejs.com/docs/lambda.html
