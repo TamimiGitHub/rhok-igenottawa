e1a5c3232ad7d6a5bc8a7ef2e6cd9a5902bb372b
Tamimi <ahmadtamimi.mail@gmail.com>
2019-05-05 14:37:26 -0400

    add more card content

--- functions/list.js
+++ functions/list.js
@@ -98,8 +98,11 @@ function renderEvent(event) {
     <img class="card-img-top" src="/images/${randomImage()}" alt="Card image cap">
     <div class="card-body">
       <p class="card-text"><b>${event.eventTitle}</b></p>
+      <p class="card-text">${event.website}</p>
+      <!-- <p class="card-text">${event.organizationName}</p> -->
       <p class="card-text">${dateFormat(Date(event.date), "dddd, mmmm dS, yyyy")}</p>
       <p class="card-text">${event.startTimeHour}:${event.startTimeMinute} - ${event.endTimeHour}:${event.endTimeMinute}</p>
+      <!-- <p class="card-text">${event.location}</p> -->
       <p class="card-text">---------</p>
       <p class="card-text">${event.eventDesc}</p>
       <div class="d-flex justify-content-between align-items-center">
