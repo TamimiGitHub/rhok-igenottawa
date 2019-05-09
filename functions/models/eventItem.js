d668bcbf624d86a6c9057f3184da768eae094dc3
Tai Dickerson <19917125+tdickers@users.noreply.github.com>
2019-05-08 23:25:00 -0400

    Change schema back to production

--- functions/models/eventItem.js
+++ functions/models/eventItem.js
@@ -1,8 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-// const collectionName = 'events'
-const collectionName = 'igen'
+const collectionName = 'events'
 
 // Create Schema
 var eventSchema = new Schema({
